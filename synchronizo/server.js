be86a373fb973aa6a26a0e0e9571b96c06671085
lee1937 <lee1937@purdue.edu>
2017-02-19 15:19:08 -0500

    Eric's First Change

--- synchronizo/server.js
+++ synchronizo/server.js
@@ -2,7 +2,11 @@ var express    =    require('express');
 var app        =    express();
 
 app.get('/',function(req,res){
-    res.send('Hello world');
+    res.send('Hello world\n This is a test\n');
+});
+
+app.get('/foo/bar',function(req,res){
+    res.send('Hello world\n This is a test\n');
 });
 
 var server     =    app.listen(8080 ,function(){
