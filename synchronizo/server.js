511dc592f586586f74a8b55cde52f1ad57769cb6
Ammar Askar <ammar@ammaraskar.com>
2017-02-28 23:11:48 -0500

    Add initial templating work

--- synchronizo/server.js
+++ synchronizo/server.js
@@ -6,14 +6,17 @@ var app = express();
 nunjucks.configure('views', {
     autoescape: true,
     throwOnUndefined: true,
+    noCache: true,
     express: app
 });
 
+app.use(express.static('public'));
+
 app.get('/',function(req, res){
-    res.render('index.html');
+    res.render('public/index.html');
 });
 
-var server     =    app.listen(8080 ,function(){
+var server = app.listen(8080 ,function(){
     console.log("We have started our server on port 8080");
 });
 
